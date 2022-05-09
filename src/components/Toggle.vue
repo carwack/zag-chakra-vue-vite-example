ac227065ce387c822956af7fe7d9dd6a6d3fa64f
carwack <sybren.willemot@gmail.com>
2022-05-09 16:22:46 +0200

    feat: add toggle component

---
+++ src/components/Toggle.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from "vue"
+import * as toggle from "@zag-js/toggle"
+import { CButton } from "@chakra-ui/vue-next"
+import { useMachine, useSetup, normalizeProps } from "@zag-js/vue"
+
+const [state, send] = useMachine(toggle.machine)
+const ref = useSetup({ send, id: "1" })
+const api = computed(() => toggle.connect(state.value, send, normalizeProps))
+</script>
+
+<template>
+  <c-button ref="ref" v-bind="api.buttonProps">
+    {{ api.isPressed }}
+  </c-button>
+</template>
